fe8aaa1678e985950852ff2b6b225eb008994250
Christine <stinethebean@users.noreply.github.com>
2017-11-17 15:26:37 -0800

    Updating to spelling my name correctly

--- data/hackSpecificData.js
+++ data/hackSpecificData.js
@@ -6,49 +6,9 @@ module.exports = {
   boothLocation: 'TBD',
   teamMembers: [
     {
-      name: 'Chrsitien',
+      name: 'Christine Matheney',
       techFocus: ['Azure', 'AI', 'Data Science'],
       photoLink: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAx2AAAAJGZjZDUwYmEyLTVlNjAtNDk5YS05MWI1LTJjODlhNGVlODM1Mw.jpg'
-    },
-    {
-      name: 'Alina',
-      techFocus: ['Java', 'Python'],
-      photoLink: 'https://raw.githubusercontent.com/jcocchi/HannahHackathon/master/images/Alina.jpg'
-    },
-    {
-      name: 'Davies',
-      techFocus: ['Algorithms', 'NLP', 'UI/UX', 'Web Dev'],
-      photoLink: 'https://raw.githubusercontent.com/jcocchi/HannahHackathon/master/images/Davies.png'
-    },
-    {
-      name: 'Justine',
-      techFocus: ['Bot Framework', 'Cognitive Services', 'Web Dev', 'JavaScript'],
-      photoLink: 'https://raw.githubusercontent.com/jcocchi/HannahHackathon/master/images/Justine.jpg'
-    },
-    {
-      name: 'Pablo',
-      techFocus: ['Azure', 'IaaS', 'Web Dev'],
-      photoLink: 'https://raw.githubusercontent.com/jcocchi/HannahHackathon/master/images/Pablo.jpg'
-    },
-    {
-      name: 'Sara',
-      techFocus: ['Python', 'C#', 'Cognitive Services', 'Azure Media Services'],
-      photoLink: 'https://raw.githubusercontent.com/jcocchi/HannahHackathon/master/images/Sara.jpg'
-    },
-    {
-      name: 'Shan',
-      techFocus: ['Ruby on Rails', 'Java', 'Python', 'Distributed Systems'],
-      photoLink: 'https://raw.githubusercontent.com/jcocchi/HannahHackathon/master/images/Shan.jpg'
-    },
-    {
-      name: 'Tim',
-      techFocus: ['Azure', 'Blockchain', 'JavaScript', 'Mongo DB', 'C#'],
-      photoLink: 'https://raw.githubusercontent.com/jcocchi/HannahHackathon/master/images/Tim.jpg'
-    },
-    {
-      name: 'Yashika',
-      techFocus: ['General Microsoft Tech'],
-      photoLink: 'https://raw.githubusercontent.com/jcocchi/HannahHackathon/master/images/Yashika.png'
     }
   ]
 }
